921ffd76f13a1d8b4384324ec31e38627d43f4e8
Rael <allice2283006@gmail.com>
2025-06-03 21:44:15 +0900

    Time: 2 ms (97.27%), Space: 41.9 MB (48.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') return false;
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') return false;
+                    break;
+                default:
+                    stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
